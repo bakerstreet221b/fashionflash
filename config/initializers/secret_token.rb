5b988a95eb44da5ae581ee1fa0c1a5d8e11d5390
Theres Scherrer <theresscherrer@gmail.com>
2013-04-23 09:48:10 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fashionflash::Application.config.secret_token = '472086ce19015b5ba360bc66a130cab71fecb3038b19bf8459ee6114ba5a8545af14b3d3c94db2fe6d0676e4d009e2d6ee888f74a9537401d3cbdce69daedff2'
